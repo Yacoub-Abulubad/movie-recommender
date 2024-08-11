a1f764ece2a3fff328514a3d1e0cd82b5f285c88
Yacoub <yacoub.yousef@orbem.ai>
2024-08-11 21:07:14 +0200

    Comment removed fr

--- movie-recommender/src/components/Header/index.js
+++ movie-recommender/src/components/Header/index.js
@@ -4,5 +4,3 @@ import RMDBLogo from "../../images/react-movie-logo.svg";
 import TMDBLogo from "../../images/tmdb_logo.svg";
 
 import { Wrapper, Content, LogoImg, TMDBLogoImg } from "./Header.styles";
-
-// Comment removed?
