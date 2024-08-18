a5f87175cdd12140837bf9ef88cde8ccc5c3fdac
Yacoub <yacoub.abulubad@gmail.com>
2024-08-18 17:11:55 +0200

    HeroImage added to home

--- movie-recommender/src/components/Home.js
+++ movie-recommender/src/components/Home.js
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL, API_KEY } from "../config";
 
 //components
+import HeroImage from "./HeroImage";
 
 //hook
 import { useHomeFetch } from "../hooks/useHomeFetch";
@@ -14,7 +15,17 @@ import NoImage from "../images/no_image.jpg";
 const Home = () => {
   const { state, loading, error } = useHomeFetch();
   console.log(state);
-  return <div>Home Page</div>;
+  return (
+    <>
+      {state.results[0] ? (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      ) : null}
+    </>
+  );
 };
 
 export default Home;
