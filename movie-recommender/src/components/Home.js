a57edc23af36e0e9ee8b85544772161fe01777d4
Yacoub <yacoub.abulubad@gmail.com>
2024-08-18 16:43:50 +0200

    Fetch movies API

--- movie-recommender/src/components/Home.js
+++ movie-recommender/src/components/Home.js
@@ -1,20 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 //config
-import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL, API_KEY } from "../config";
 
 //components
 
 //hook
+import { useHomeFetch } from "../hooks/useHomeFetch";
 
 //image
 import NoImage from "../images/no_image.jpg";
 
 const Home = () => {
-  const [state, setState] = useState();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
+  const { state, loading, error } = useHomeFetch();
+  console.log(state);
   return <div>Home Page</div>;
 };
 
